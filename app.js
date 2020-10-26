93cbe9cf3da254ccc9601daf9a5c60e00ed0779e
nosideri <madi.stollmeyer@gmail.com>
2020-10-25 17:59:49 -0700

    added rest of functions needed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql")
 var inquirer = require("inquirer")
- 
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
     host:"localhost",
@@ -17,53 +17,94 @@ connection.connect(function(err) {
 })
 
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-function start() {
+async function start() {
     inquirer.prompt({
         type: "list",
         message: "What would you like to do?",
-        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+        choices: ["View all departments", "View all roles", "View all employees", "View employee names", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
         name: "selection"
     }).then(function(userinput){
         console.log(userinput.selection)
         if(userinput.selection == "View all departments") {
             // show departments only
             viewDepartment();
-            break;
+            
         } else if (userinput.selection == "View all roles") {
             //show roles only
-            viewRole();
-            break;
+            viewRoles();
+            
         } else if (userinput.selection == "View all employees") {
             //show employees only
             viewEmployees();
-            break;
+            
+        } else if (userinput.selection == "View employee names") {
+            //show employee names
+            viewEmployeeNames();
+            
         } else if (userinput.selection == "Add a department") {
             //add another department
             addDepartment();
-            break;
+
         } else if (userinput.selection == "Add a role") {
             //add another role
             addRole();
-            break;
-        } else if (userinput.selection == "Add an employee") {
+            
+        }  else if (userinput.selection == "Add an employee") {
             //add nanother employee
             addEmployee();
-            break;
+            
         } else if (userinput.selection == "Update an employee role") {
             //update an existing employee
             updateEmployee();
-            break;
+            
         }
     })
 };
 
-function addDepartment() {
+async function viewDepartment() {
+    let query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+async function viewRoles() {
+    let query = "SELECT title, role.id, department_id, salary, department.name FROM role, department";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+async function viewEmployees() {
+    let query = "SELECT employee.id, first_name, last_name, role.title, department.name, role.salary, manager_id FROM employee, role, department";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+async function viewEmployeeNames() {
+    let query = "SELECT first_name, last_name FROM employee";
+
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        start();
+    })
+}
+
+async function addDepartment() {
     inquirer.prompt({
         type: "input",
         message: "What is the name of the new department?",
         name: "deptName"
     }).then(function(response){
-        connection.query("INSERT INTO department (name) VALUES (?)", [answer.deptName], function(err, res) {
+        connection.query("INSERT INTO department (name) VALUES (?)", [response.deptName], function(err, res) {
             if (err) throw err;
             console.table(res)
             start();
@@ -71,7 +112,7 @@ function addDepartment() {
     })
 }
 
-function addRole() {
+async function addRole() {
     inquirer.prompt([
         {
             type: "input",
@@ -95,4 +136,58 @@ function addRole() {
             start();
         })
     })
+}
+
+async function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Whats the first name of the employee?",
+            name: "newEmpFN"
+        },
+        {
+            type: "input",
+            message: "Whats the last name of the employee?",
+            name: "newEmpLN"
+        },
+        {
+            type: "input",
+            message: "What is the employees role ID number?",
+            name: "roleID"
+        },
+        {
+            type: "input",
+            message: "What is the employees managers ID number?",
+            name: "managerID"
+        }
+    ]).then(function(response) {
+        connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [response.newEmpFN, response.newEmpLN, response.roleID, response.managerID], function(err, res) {
+            if (err) throw err;
+            console.table(res);
+            start();
+        })
+    })
+}
+
+async function updateEmployee() {
+    let employees = viewEmployeeNames();
+    let roles = viewRoles();
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Which employee would you like to update?",
+            name: "empUpdate",
+            choices: [ employees ]
+        },
+        {
+            type: "list",
+            message: "What is the employees new role?",
+            name: "updateRole",
+            choices: [ roles ]
+        }
+    ]).then(function(response) {
+            console.table(res);
+            start();
+        })
 }
